b624def025550c8bffd088c74b7dd936d41d1a9c
Jakub Derda <jakub@kobietydokodu.pl>
2017-08-01 19:05:10 +0100

    Konfiguracja Spring Security z baza danych

--- src/main/java/pl/kobietydokodu/bilet/config/SecurityConfig.java
+++ src/main/java/pl/kobietydokodu/bilet/config/SecurityConfig.java
@@ -1,20 +1,35 @@
 package pl.kobietydokodu.bilet.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	DataSource dataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin@admin.pl")
-			.password("admin")
-			.roles("USER");
+		auth.jdbcAuthentication()
+			.dataSource(dataSource)
+			.usersByUsernameQuery("SELECT `email`,`passwordHash`,`active` FROM `users` WHERE `email`=?")
+			.authoritiesByUsernameQuery("SELECT `email`,'USER' FROM `users` WHERE `email`=?")
+			.passwordEncoder(passwordEncoder());
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
