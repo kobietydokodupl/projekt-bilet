d78bbd2bd030f5c8345f253dd129657546e3669c
Jakub Derda <jakub@kobietydokodu.pl>
2017-08-01 18:28:02 +0100

    Podstawowa konfiguracja Spring Security

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 * [Lekcja 0 - wstęp](http://kobietydokodu.pl/projekt-bilet-0-zaczynamy/)
 * [Lekcja 1 - pierwsze linijki kodu](http://kobietydokodu.pl/projekt-bilet-1-pierwsze-linijki-kodu/)
 * [Lekcja 2 - konfiguracja bazy danych](http://kobietydokodu.pl/projekt-bilet-2-konfiguracja-bazy-danych/)
+* [Lekcja 3 - konfiguracja Spring Security oraz OAuth](http://kobietydokodu.pl/projekt-bilet-3-konfigurujemy-spring-security-oraz-oauth/)
---
+++ src/main/java/pl/kobietydokodu/bilet/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package pl.kobietydokodu.bilet.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin@admin.pl")
+			.password("admin")
+			.roles("USER");
+	}
+
+}
