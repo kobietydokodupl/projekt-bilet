d78c7622e6a1d006dbfaa33f412fdd66c1b45804
Jakub Derda <jakub@kobietydokodu.pl>
2017-08-06 18:54:12 +0100

    Zablokowanie 'standardowego' Spring Security

--- src/main/java/pl/kobietydokodu/bilet/config/SecurityConfig.java
+++ src/main/java/pl/kobietydokodu/bilet/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,5 +42,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .anyRequest()
+                    .denyAll()
+                .and()
+            .formLogin()
+                .disable();
+    }
 
 }
